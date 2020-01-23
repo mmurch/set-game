8fd1303ab2a4d628547fd7ebca572cf04087cbdb
Matthew Murchison <murchigras@gmail.com>
2020-01-23 12:04:51 -0500

    some slightly better, but still bad, styling for the summary grid

--- app/static/src/js/summary.jsx
+++ app/static/src/js/summary.jsx
@@ -6,30 +6,41 @@ function Summary(props) {
     let summaryCards = [];
     for (let i = 0; i < 6; i++) {
         summaryCards.push(
-            <div className="one-third column" key={i}>
-                <div className="one-third column card">&nbsp;</div>
-                <div className="one-third column card">&nbsp;</div>
-                <div className="one-third column card">&nbsp;</div>
-            </div>)
+            <td className="summary-set" key={i}>
+                <div className="card summary-card">&nbsp;</div>
+                <div className="card summary-card">&nbsp;</div>
+                <div className="card summary-card">&nbsp;</div>
+            </td>)
     }
 
     for (let i = 0; i < props.setsFound.length; i++) {
         let summarySet =
-            <div className="one-third column" key={props.setsFound[i][0] + '_' + props.setsFound[i][1] + '_' + props.setsFound[i][2]}>
-                <div className="one-third column card"><img src={'/static/img/card_' + props.setsFound[i][0] + '.png'} /></div>
-                <div className="one-third column card"><img src={'/static/img/card_' + props.setsFound[i][1] + '.png'} /></div>
-                <div className="one-third column card"><img src={'/static/img/card_' + props.setsFound[i][2] + '.png'} /></div>
-            </div>;
+            <td className="summary-set" key={props.setsFound[i][0] + '_' + props.setsFound[i][1] + '_' + props.setsFound[i][2]}>
+                <div className="card summary-card">
+                    <img src={'/static/img/card_' + props.setsFound[i][0] + '.png'} />
+                </div>
+                <div className="card summary-card">
+                    <img src={'/static/img/card_' + props.setsFound[i][1] + '.png'} />
+                </div>
+                <div className="card summary-card">
+                    <img src={'/static/img/card_' + props.setsFound[i][2] + '.png'} />
+                </div>
+            </td>;
         summaryCards.splice(i, 1, summarySet);
     }
 
     return (
-        <div className="summary row">
-            <div className="three columns">&nbsp;</div>
-            <div className="six columns">
-                {summaryCards}
-            </div>
-            <div className="three columns">&nbsp;</div>
+        <div className="row">
+            <table className="summary">
+                <tbody>
+                    <tr className="summary-set-row">
+                        {summaryCards.slice(0, 3)}
+                    </tr>
+                    <tr className="summary-set-row">
+                        {summaryCards.slice(3, 6)}
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
